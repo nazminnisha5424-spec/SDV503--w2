36b6f5b1cf3f2d395e772910b2c0894e988bf444
Nazmin N Kata <nazminnisha5424@gmail.com>
2026-03-02 11:29:58 +1300

    created a js file named app

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+
+var name = "Alice";//Function-scoped, hoisted - avoid in modern J5
+let age = 25;//Block-scoped, can be reassigned
+const pi = 3.14159;//Block-scoped, cannot be reassigned
+
+let scrore = 0;
+scrore = 10;//OK - reassignment allowed for let
+
+const MAX = 100;
+//MAX = 200; //TypeErroer: Assignment to constant variable.
+let homeaddress = "Your home address";
