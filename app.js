21f17ae998ed5aac3128a96bb9cd1f0f93e94b97
Nazmin N Kata <nazminnisha5424@gmail.com>
2026-03-04 11:07:54 +1300

    let a = 10;

--- app.js
+++ app.js
@@ -9,3 +9,31 @@ scrore = 10;//OK - reassignment allowed for let
 const MAX = 100;
 //MAX = 200; //TypeErroer: Assignment to constant variable.
 let homeaddress = "Your home address";
+cosole.log(typof homeaddress); 
+//Arithmetic 
+5 + 3 // 8 
+10 - 4 // 6 
+4 * 3 // 12 
+9 / 2 // 4.5 
+9 % 2 // 1 
+2**3 // 8
+
+//comperison - aways prefare === over ==
+5 === 5 // true
+5 !== 3 // true // (strict check value and type)
+5 == "5" // true
+5 === "5" // false// (loose vs strict) 
+10 > 3 // true
+4 <= 4 // true
+
+//logical
+true && false // false
+true || false // true
+!true // false
+
+//assignment shorthands 
+let x = 10;
+x += 5; //15
+x -= 2; // 13
+x *=2; //26
+
